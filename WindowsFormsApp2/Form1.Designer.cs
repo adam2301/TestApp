d8508be8f33580a638538ddfd13dd7bc7ce943d0
damirdzh <zarazzza.a401@gmail.com>
2023-01-22 01:31:37 +0300

    Цвет фона

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -64,10 +64,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
